04ba01055b85c707afea96336feb777b232a379d
Amber Lu <amber.lu@ubisoft.com>
2022-03-20 17:09:46 -0300

    users list and create a new user

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+  const addUserHandler = (username, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: username, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
   return (
-    <div className="App">
-      <h2>React</h2>
+    <div>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
     </div>
   );
 }
